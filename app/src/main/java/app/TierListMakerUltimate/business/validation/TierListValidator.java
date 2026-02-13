0b65000b4fa9b2a2b7cd501b4413dbcd3e998aaa
Sakeef Ahmed <ahmeds45@myumanitoba.ca>
2026-02-13 00:26:55 -0600

    Refactored TierListValidator to better respect SRP and be consistent with other validator calsses

--- app/src/main/java/app/TierListMakerUltimate/business/validation/TierListValidator.java
+++ app/src/main/java/app/TierListMakerUltimate/business/validation/TierListValidator.java
@@ -5,12 +5,20 @@ import app.TierListMakerUltimate.business.exception.ValidationException;
 public class TierListValidator {
 
     public void validateCreateTierList(String name) {
+        validateName(name);
+    }
+
+    public void validateName(String name) {
         if (name == null || name.trim().isEmpty()) {
             throw new ValidationException("Valid name is Required");
         }
     }
 
     public void validateRemoveTierList(int tierListId) {
+        validateTierListId(tierListId);
+    }
+
+    public void validateTierListId(int tierListId) {
         if (tierListId <= 0) {
             throw new ValidationException("TierList Id is required");
         }
