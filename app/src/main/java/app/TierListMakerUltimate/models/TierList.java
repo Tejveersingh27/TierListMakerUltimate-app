ecb3772b8fefb9d21e09a0e2a291207eecf147c7
Sakeef Ahmed <ahmeds45@myumanitoba.ca>
2026-03-06 13:56:45 -0600

    added thumbnail image variable to tierlists

--- app/src/main/java/app/TierListMakerUltimate/models/TierList.java
+++ app/src/main/java/app/TierListMakerUltimate/models/TierList.java
@@ -5,17 +5,28 @@ import java.util.Objects;
 public class TierList {
     private int id;
     private String name;
+    private String thumbnailImagePath;
 
     // For new tiers
     public TierList(String name) {
         this.name = name;
     }
 
+    public TierList(String name, String thumbnailImagePath) {
+        this.name = name;
+        this.thumbnailImagePath = thumbnailImagePath;
+    }
+
     // For DB load
-    public TierList(int id, String name) {
+    public TierList(int id, String name, String thumbnailImagePath) {
         this.id = id;
         this.name = name;
     }
+
+    public String getThumbnailImagePath() {
+        return this.thumbnailImagePath;
+    }
+
 
     public int getId() {
         return this.id;
