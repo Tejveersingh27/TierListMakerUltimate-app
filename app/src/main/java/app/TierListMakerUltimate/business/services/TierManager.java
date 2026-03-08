4cd632dc214ba675b70944881583f26bc5fafe61
Sakeef Ahmed <ahmeds45@myumanitoba.ca>
2026-03-08 10:11:43 -0500

    TierManager now also respects DRY by using the same patterns seen in ItemPlacementManager & TierListManager.

--- app/src/main/java/app/TierListMakerUltimate/business/services/TierManager.java
+++ app/src/main/java/app/TierListMakerUltimate/business/services/TierManager.java
@@ -38,31 +38,29 @@ public class TierManager implements ITierManager {
     @Override
     public void removeTier(int tierId) throws ValidationException, NotFoundException {
         validator.validateRemoveTier(tierId);
-        if (tierStorage.getTier(tierId) == null) {
-            throw new NotFoundException(ERROR_TIER_NOT_FOUND + tierId);
-        }
+        getVerifiedTier(tierId);
         tierStorage.deleteTier(tierId);
     }
 
     @Override
     public Tier getTier(int tierId) throws ValidationException, NotFoundException {
         validator.validateTierId(tierId);
-        Tier tier = tierStorage.getTier(tierId);
-        if (tier == null) {
-            throw new NotFoundException(ERROR_TIER_NOT_FOUND + tierId);
-        }
-        return tier;
+        return getVerifiedTier(tierId);
     }
 
     @Override
     public void updateTier(Tier updatedTier) throws ValidationException, NotFoundException {
         validator.validateUpdateTier(updatedTier);
-
-        if (tierStorage.getTier(updatedTier.getId()) == null) {
-            throw new NotFoundException(ERROR_TIER_NOT_FOUND + updatedTier.getId());
-        }
-
+        getVerifiedTier(updatedTier.getId());
         tierStorage.updateTier(updatedTier);
+    }
+
+    private Tier getVerifiedTier(int tierId) throws NotFoundException {
+        Tier tier = tierStorage.getTier(tierId);
+        if (tier == null) {
+            throw new NotFoundException(ERROR_TIER_NOT_FOUND + tierId);
+        }
+        return tier;
     }
 
     @Override
